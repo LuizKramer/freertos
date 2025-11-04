20c19854857b0c960d0127b223400455dd45e75c
LuizKramer <luizkramer.dev@gmail.com>
2025-11-04 13:11:44 -0300

    Add send serial from ISR

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,31 +1,30 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
-#include "stm32g4xx_hal_gpio.h"
+#include "portmacro.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <stdint.h>
 #include <string.h>
-
-
 
 /* USER CODE END Includes */
 
@@ -37,11 +36,11 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 
-typedef struct led_params_t_{
-  GPIO_TypeDef 	*led_port;
-	uint16_t 		led_pin;
-	TickType_t 		led_toggle_time;
-}led_params_t;
+typedef struct led_params_t_ {
+  GPIO_TypeDef *led_port;
+  uint16_t led_pin;
+  TickType_t led_toggle_time;
+} led_params_t;
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -58,69 +57,97 @@ SemaphoreHandle_t sem = NULL;
 SemaphoreHandle_t mutex = NULL;
 TimerHandle_t debounce;
 QueueHandle_t queue_keyb;
+QueueHandle_t serial_queue;
 volatile uint32_t counter = 0;
 
-void blink_led(void* param){
- 	led_params_t *led = (led_params_t *)param;
-	TickType_t timer;
-	while(1){
-		timer = xTaskGetTickCount();
-		HAL_GPIO_TogglePin(led->led_port, led->led_pin);
-		vTaskDelayUntil(&timer,led->led_toggle_time);
-	}
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
+  BaseType_t pxHigherPriorityTaskWoken = pdFALSE;
+  xSemaphoreGiveFromISR(sem, &pxHigherPriorityTaskWoken);
+  portYIELD_FROM_ISR(pxHigherPriorityTaskWoken);
 }
 
-
-void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){
-	BaseType_t pxHigherPriorityTaskWoken = pdFALSE;
-	xSemaphoreGiveFromISR(sem, &pxHigherPriorityTaskWoken);
-	portYIELD_FROM_ISR(pxHigherPriorityTaskWoken);
+void blink_led(void *param) {
+  led_params_t *led = (led_params_t *)param;
+  TickType_t timer;
+  while (1) {
+    timer = xTaskGetTickCount();
+    HAL_GPIO_TogglePin(led->led_port, led->led_pin);
+    vTaskDelayUntil(&timer, led->led_toggle_time);
+  }
 }
 
 volatile uint32_t cnt1 = 0;
-void envia_serial_1(void *param){
-	char *texto = "Olá mundo da tarefa 1!\n\r";
-	uint32_t len = strlen(texto);
-	while(1){
-		if (xSemaphoreTake(mutex, 100) == pdTRUE){
-			if (HAL_UART_Transmit_IT(&hlpuart1, (uint8_t *)texto, len) == HAL_OK){
-				xSemaphoreTake(sem, portMAX_DELAY);
-				cnt1++;
-			}
-			xSemaphoreGive(mutex);
-		}
-		taskYIELD();
-	}
+void envia_serial_1(void *param) {
+  char *texto = "Olá mundo da tarefa 1!\n\r";
+  uint32_t len = strlen(texto);
+  while (1) {
+    if (xSemaphoreTake(mutex, 100) == pdTRUE) {
+      if (HAL_UART_Transmit_IT(&hlpuart1, (uint8_t *)texto, len) == HAL_OK) {
+        xSemaphoreTake(sem, portMAX_DELAY);
+        cnt1++;
+      }
+      xSemaphoreGive(mutex);
+    }
+    taskYIELD();
+  }
 }
 
 volatile uint16_t ctn = 0;
-void send_serial(void * param){
-  char* texto = (char*) param;
-	uint32_t len = strlen(texto);
-	while(1){
-		if (xSemaphoreTake(mutex, 100) == pdTRUE){
-			if (HAL_UART_Transmit_IT(&hlpuart1, (uint8_t *)texto, len) == HAL_OK){
-				xSemaphoreTake(sem, portMAX_DELAY);
+void send_serial(void *param) {
+  char *texto = (char *)param;
+  uint32_t len = strlen(texto);
+  while (1) {
+    if (xSemaphoreTake(mutex, 100) == pdTRUE) {
+      if (HAL_UART_Transmit_IT(&hlpuart1, (uint8_t *)texto, len) == HAL_OK) {
+        xSemaphoreTake(sem, portMAX_DELAY);
         ctn++;
-			}
-			xSemaphoreGive(mutex);
-		}
-		taskYIELD();
-	}
+      }
+      xSemaphoreGive(mutex);
+    }
+    taskYIELD();
+  }
+}
+void uart_transmit(char *str, uint32_t len) {
+  if (xSemaphoreTake(mutex, 100) == pdTRUE) {
+    if (HAL_UART_Transmit_IT(&hlpuart1, (uint8_t *)str, len) == HAL_OK) {
+      xSemaphoreTake(sem, portMAX_DELAY);
+      ctn++;
+    }
+    xSemaphoreGive(mutex);
+  }
 }
 
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
-	BaseType_t pxHigherPriorityTaskWoken = pdFALSE;
-	HAL_NVIC_DisableIRQ(EXTI15_10_IRQn);
-	xTimerStartFromISR(debounce, &pxHigherPriorityTaskWoken);
-  HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-	portYIELD_FROM_ISR(pxHigherPriorityTaskWoken);
+void print_serial(void *param) {
+    char msg[64];
+
+    while (1) {
+        // Wait for message from queue
+        if (xQueueReceive(serial_queue, &msg, portMAX_DELAY) == pdTRUE) {
+            uart_transmit(msg, strlen(msg));
+        }
+    }
 }
 
-void debounce_callback( TimerHandle_t xTimer ){
-	uint8_t buffer = 1;
-	xQueueSendToBack(queue_keyb, &buffer, 0);
-	HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+    BaseType_t pxHigherPriorityTaskWoken = pdFALSE;
+    HAL_NVIC_DisableIRQ(EXTI15_10_IRQn);
+    xTimerStartFromISR(debounce, &pxHigherPriorityTaskWoken);
+
+    HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+
+    const char *string = "Hello from IRQ\r\n";
+
+    xQueueSendFromISR(serial_queue, string, &pxHigherPriorityTaskWoken);
+
+    portYIELD_FROM_ISR(pxHigherPriorityTaskWoken);
+}
+
+void debounce_callback(TimerHandle_t xTimer) {
+  uint8_t buffer = 1;
+  xQueueSendToBack(queue_keyb, &buffer, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 }
 /* USER CODE END PV */
 
@@ -128,7 +155,7 @@ void debounce_callback( TimerHandle_t xTimer ){
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_LPUART1_UART_Init(void);
-void StartDefaultTask(void const * argument);
+void StartDefaultTask(void const *argument);
 
 /* USER CODE BEGIN PFP */
 
@@ -140,11 +167,10 @@ void StartDefaultTask(void const * argument);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
 
   /* USER CODE BEGIN 1 */
 
@@ -152,7 +178,8 @@ int main(void)
 
   /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+   */
   HAL_Init();
 
   /* USER CODE BEGIN Init */
@@ -176,7 +203,8 @@ int main(void)
   /* USER CODE BEGIN RTOS_MUTEX */
   mutex = xSemaphoreCreateMutex();
   sem = xSemaphoreCreateBinary();
-  debounce = xTimerCreate("debounce do teclado", 50, pdFALSE, NULL, debounce_callback);
+  debounce =
+      xTimerCreate("debounce do teclado", 50, pdFALSE, NULL, debounce_callback);
   /* USER CODE END RTOS_MUTEX */
 
   /* USER CODE BEGIN RTOS_SEMAPHORES */
@@ -189,6 +217,7 @@ int main(void)
 
   /* USER CODE BEGIN RTOS_QUEUES */
   queue_keyb = xQueueCreate(16, sizeof(char));
+  serial_queue = xQueueCreate(8, sizeof(char[64]));
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
@@ -200,14 +229,16 @@ int main(void)
   // static led_params_t led1 = {
   //   LED_GPIO_Port,
   //    LED_Pin,
-  //    500 
+  //    500
   // };
 
-  //char * string = "Hello from task1";
-  //xTaskCreate(blink_led, "Blink LED", 256, &led1, 4, NULL);
-  //xTaskCreate(send_serial, "Serial1",256, string, 3, NULL);
-  xTaskCreate(envia_serial_1, "serial 1", 256, NULL, 3, NULL);
-  
+  // char * string = "Hello from task1\n\r";
+  // char * string2 = "Hello from task2\n\r";
+  // xTaskCreate(blink_led, "Blink LED", 256, &led1, 4, NULL);
+  // xTaskCreate(send_serial, "Serial1",256, string, 3, NULL);
+  // xTaskCreate(send_serial, "Serial1",256, string2, 3, NULL);
+  // xTaskCreate(envia_serial_1, "serial 1", 256, NULL, 6, NULL);
+  xTaskCreate(print_serial, "Print Serial", 256, NULL, 1, NULL);
   /* USER CODE END RTOS_THREADS */
 
   /* Start scheduler */
@@ -217,8 +248,7 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  while (1) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -227,21 +257,20 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -251,33 +280,30 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
   RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
     Error_Handler();
   }
 }
 
 /**
-  * @brief LPUART1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_LPUART1_UART_Init(void)
-{
+ * @brief LPUART1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_LPUART1_UART_Init(void) {
 
   /* USER CODE BEGIN LPUART1_Init 0 */
 
@@ -287,7 +313,7 @@ static void MX_LPUART1_UART_Init(void)
 
   /* USER CODE END LPUART1_Init 1 */
   hlpuart1.Instance = LPUART1;
-  hlpuart1.Init.BaudRate = 209700;
+  hlpuart1.Init.BaudRate = 115200;
   hlpuart1.Init.WordLength = UART_WORDLENGTH_8B;
   hlpuart1.Init.StopBits = UART_STOPBITS_1;
   hlpuart1.Init.Parity = UART_PARITY_NONE;
@@ -296,35 +322,31 @@ static void MX_LPUART1_UART_Init(void)
   hlpuart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
   hlpuart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
   hlpuart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&hlpuart1) != HAL_OK)
-  {
+  if (HAL_UART_Init(&hlpuart1) != HAL_OK) {
     Error_Handler();
   }
-  if (HAL_UARTEx_SetTxFifoThreshold(&hlpuart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
-  {
+  if (HAL_UARTEx_SetTxFifoThreshold(&hlpuart1, UART_TXFIFO_THRESHOLD_1_8) !=
+      HAL_OK) {
     Error_Handler();
   }
-  if (HAL_UARTEx_SetRxFifoThreshold(&hlpuart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
-  {
+  if (HAL_UARTEx_SetRxFifoThreshold(&hlpuart1, UART_RXFIFO_THRESHOLD_1_8) !=
+      HAL_OK) {
     Error_Handler();
   }
-  if (HAL_UARTEx_DisableFifoMode(&hlpuart1) != HAL_OK)
-  {
+  if (HAL_UARTEx_DisableFifoMode(&hlpuart1) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN LPUART1_Init 2 */
 
   /* USER CODE END LPUART1_Init 2 */
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
   /* USER CODE BEGIN MX_GPIO_Init_1 */
 
@@ -366,37 +388,33 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN Header_StartDefaultTask */
 /**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
+ * @brief  Function implementing the defaultTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void const * argument)
-{
+void StartDefaultTask(void const *argument) {
   /* USER CODE BEGIN 5 */
   /* Infinite loop */
-  for(;;)
-  {
+  for (;;) {
     osDelay(1);
   }
   /* USER CODE END 5 */
 }
 
 /**
-  * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM7 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM7 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
   /* USER CODE BEGIN Callback 0 */
 
   /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM7)
-  {
+  if (htim->Instance == TIM7) {
     HAL_IncTick();
   }
   /* USER CODE BEGIN Callback 1 */
@@ -405,32 +423,30 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 }
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
+  while (1) {
   }
   /* USER CODE END Error_Handler_Debug */
 }
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* User can add his own implementation to report the file name and line
+     number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
+     line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
